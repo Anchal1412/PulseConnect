51f4f020649b807105478cb812f18190071f44b6
Anchal1412 <singhaj8130@gmail.com>
2026-03-17 09:46:53 +0530

    add  user schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/baseSystem'),
+    UserModule,
   ],
 })
 export class AppModule {}
---
+++ backend/src/user/schemas/user.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+
+  @Prop()
+  name: string;
+
+  @Prop({ unique: true })
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop({ default: true })
+  status: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
