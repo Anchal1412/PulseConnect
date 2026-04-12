73b52856e90ba3c8bbfe0129e71becd65e5811ca
Anchal Singh <your-email@example.com>
2026-04-12 15:55:38 +0530

    fix type:authController

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,7 +2,8 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
-
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { UserDocument } from '../user/schemas/user.schema';
 @Injectable()
 export class AuthService {
   constructor(
@@ -13,20 +14,14 @@ export class AuthService {
   async login(
     email: string,
     password: string,
-  ): Promise<{
-    id: string;
-    name: string;
-    email: string;
-    token: string;
-    roomId: string;
-  }> {
-    const user = await this.userService.findByEmail(email);
+  ): Promise<Omit<CreateUserDto, 'password'> & { token: string }> {
+    const user: UserDocument | null = await this.userService.findByEmail(email);
 
     if (!user) {
       throw new UnauthorizedException('User not found');
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch: boolean = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
       throw new UnauthorizedException('Invalid password');
@@ -39,10 +34,9 @@ export class AuthService {
       roomId: user.roomId,
     };
 
-    const token = this.jwtService.sign(payload);
+    const token: string = this.jwtService.sign(payload);
 
     return {
-      id: user._id.toString(),
       name: user.name,
       email: user.email,
       token: token,
